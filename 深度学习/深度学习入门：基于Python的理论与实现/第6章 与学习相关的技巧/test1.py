f91fb1addf98ac753bde4c0a086efcb80229d716
Ling <786173189@qq.com>
2019-07-30 18:04:50 +0800

    add filee

--- 深度学习/深度学习入门：基于Python的理论与实现/第6章 与学习相关的技巧/test1.py
+++ 深度学习/深度学习入门：基于Python的理论与实现/第6章 与学习相关的技巧/test1.py
@@ -1,16 +1,40 @@
-import matplotlib.pyplot as plt
-#from mpl_toolkits import mplot3d
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.arange(-10, 10, 0.01)
-y = np.arange(-5, 5, 0.01)
-x, y = np.meshgrid(x, y)
-z = x**2 / 20.0 + y**2
-mask = z > 7
-z[mask] = 0
 
-#ax = plt.axes(projection='3d')
-plt.contour(x, y, z)
-plt.ylim(-10, 10)
-plt.xlim(-10, 10)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    mask = x <= 0
+    x[mask] = 0
+    return x
+
+
+def tanh(x):
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+x = np.random.randn(1000, 100)  # 1000个数据
+node_num = 100  # 各隐藏层的节点(神经元)数
+hidden_layer_size = 5  # 隐藏层有5层
+activations = {}  # 激活值的结果保存在这里
+
+for i in range(hidden_layer_size):
+    if i != 0:
+        x = activations[i - 1]
+
+    w = np.random.randn(node_num, node_num) * np.sqrt(2 / node_num)
+
+    z = np.dot(x, w)
+    # a = sigmoid(z)
+    # a = tanh(z)
+    a = relu(z)
+    activations[i] = a
+
+# 绘制直方图
+for i, a in activations.items():
+    plt.subplot(1, len(activations), i + 1)
+    plt.title(str(i + 1) + "-layer")
+    plt.hist(a.flatten(), 30, range=(0, 1))
 plt.show()
